c35bb74a96d7dc0d73199deb6d69d1436626612f
João H. L. Corrêa <jhenriquelc@gmail.com>
2024-09-16 17:53:42 -0300

    Overhaul for Fedora 40 and VMWW 17.6
    - Force run as root
    - Don't download VMWare Workstation automatically as to not violate new license
    - Enforce secure boot signing
    - Auto sign after a kernel update
    - Reload kernel modules and restart services to avoid reboot

--- install.sh
+++ install.sh
@@ -1,43 +1,56 @@
 #!/usr/bin/bash
 
-orig=`pwd`
-temp=/tmp/fedora-vmware-installer/
+set -e
 
-sudo dnf update
-sudo dnf install kernel-devel kernel-headers gcc gcc-c++ make git
+if [ $UID -ne 0 ]; then
+    echo "script must be invoked as root."
+    exit 1
+fi
 
-mkdir $temp
-cd $temp
-wget --user-agent="Mozilla" --content-disposition "https://www.vmware.com/go/getplayer-linux"
-
-chmod +x VMware*.bundle
-sudo ./VMware*.bundle
-VMWARE_VERSION=$(cat /etc/vmware/config | grep player.product.version | sed '/.*\"\(.*\)\".*/ s//\1/g')
-
-git clone -b workstation-${VMWARE_VERSION} https://github.com/mkubecek/vmware-host-modules.git
-
-cd vmware-host-modules/
-make
-sudo make install
-
-sudo cp $orig/99-vmmodules.install /etc/kernel/install.d/
-sudo chmod +x /etc/kernel/install.d/99-vmmodules.install
+if [ ! -f $1 ]; then
+    echo "$1": file not found
+    echo "Please provide a VMWare Workstation 17.6 bundle as an argument."
+    exit 2
+fi
 
 if mokutil --sb-state | grep -q "SecureBoot enabled" ; then
-	if [[ -f "/etc/pki/akmods/private/private_key.priv" ]] ; then
-		echo "Assinando módulos para secure boot..."
-		sudo /usr/src/kernels/`uname -r`/scripts/sign-file sha256 /etc/pki/akmods/private/private_key.priv /etc/pki/akmods/certs/public_key.der $(modinfo -n vmmon)
-		sudo /usr/src/kernels/`uname -r`/scripts/sign-file sha256 /etc/pki/akmods/private/private_key.priv /etc/pki/akmods/certs/public_key.der $(modinfo -n vmnet)
-	else
-		echo "You're using secure boot but don't seem to have a self-signing key present. Please read the following guide to create and register a MOK:"
-		echo "https://rpmfusion.org/Howto/Secure%20Boot?highlight=%28%5CbCategoryHowto%5Cb%29"
-		echo "Or you can disable secure boot."
-	fi
-	
-	echo "You're using secure boot, you'll need to sign your keys every kernel update with the following commands:"
-	echo "sudo /usr/src/kernels/`uname -r`/scripts/sign-file sha256 /etc/pki/akmods/private/private_key.priv /etc/pki/akmods/certs/public_key.der $(modinfo -n vmmon)"
-	echo "sudo /usr/src/kernels/`uname -r`/scripts/sign-file sha256 /etc/pki/akmods/private/private_key.priv /etc/pki/akmods/certs/public_key.der $(modinfo -n vmnet)"
+    if [[ ! -f "/etc/pki/akmods/private/private_key.priv" ]] ; then
+        echo "You're using secure boot but don't seem to have a self-signing key present. Please read the following guide to create and register a MOK:"
+        echo "https://rpmfusion.org/Howto/Secure%20Boot?highlight=%28%5CbCategoryHowto%5Cb%29"
+        echo "Or you can disable secure boot."
+        exit 3
+    fi
 fi
-sudo modprobe vmmon
-sudo modprobe vmnet
 
+dnf update --refresh
+dnf install kernel-devel kernel-headers gcc gcc-c++ make git
+
+chmod +x $1
+$1
+
+pushd .
+cd /opt
+
+    git clone -b 17.6 https://github.com/bytium/vm-host-modules
+    cd vm-host-modules/
+
+    make
+    make install
+    make clean
+
+popd
+
+cp 99-vmmodules.install /etc/kernel/install.d/
+chmod +x /etc/kernel/install.d/99-vmmodules.install
+
+if mokutil --sb-state | grep -q "SecureBoot enabled" ; then
+    if [[ -f "/etc/pki/akmods/private/private_key.priv" ]] ; then
+        echo "Assinando módulos para secure boot..."
+        sudo /usr/src/kernels/`uname -r`/scripts/sign-file sha256 /etc/pki/akmods/private/private_key.priv /etc/pki/akmods/certs/public_key.der $(modinfo -n vmmon)
+        sudo /usr/src/kernels/`uname -r`/scripts/sign-file sha256 /etc/pki/akmods/private/private_key.priv /etc/pki/akmods/certs/public_key.der $(modinfo -n vmnet)
+    fi
+fi
+
+modprobe vmmon
+modprobe vmnet
+systemctl restart vmware.service vmware-USBArbitrator.service
