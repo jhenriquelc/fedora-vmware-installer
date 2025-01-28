84482121530bfa401d6f51357eedd0ff07b198e3
João H. L. Corrêa <jhenriquelc@gmail.com>
2025-01-27 21:39:37 -0300

    Format `install.sh`

--- install.sh
+++ install.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-function yes-or-no {
+function yes-or-no() {
     while true; do
         read -r -p "$* [y/n]: " yn
         case $yn in
-            [Yy]*) return 0  ;;
-            [Nn]*) return  1 ;;
+        [Yy]*) return 0 ;;
+        [Nn]*) return 1 ;;
         esac
     done
 }
@@ -18,16 +18,16 @@ if [ $UID -ne 0 ]; then
 fi
 
 # Remove old reinstallation script
-if [[ -f "/etc/kernel/install.d/99-vmmodules.install" ]] ; then
+if [[ -f "/etc/kernel/install.d/99-vmmodules.install" ]]; then
     echo "Old reinstall script detected at '/etc/kernel/install.d/99-vmmodules.install'"
-    if yes-or-no "Would you like to delete it?" ; then
+    if yes-or-no "Would you like to delete it?"; then
         rm /etc/kernel/install.d/99-vmmodules.install
     fi
 fi
 
 # SecureBoot check
-if mokutil --sb-state | grep -q "SecureBoot enabled" ; then
-    if [[ ! -f "/etc/pki/akmods/private/private_key.priv" ]] ; then
+if mokutil --sb-state | grep -q "SecureBoot enabled"; then
+    if [[ ! -f "/etc/pki/akmods/private/private_key.priv" ]]; then
         echo "You're using secure boot but don't seem to have a self-signing key present. Please read the following guide to create and register a MOK:"
         echo "https://rpmfusion.org/Howto/Secure%20Boot?highlight=%28%5CbCategoryHowto%5Cb%29"
         echo "Note that installing a MOK will trip Bitlocker encryption if you have it enabled in a Windows dual-boot, please make sure you have access to your recovery keys: https://aka.ms/myrecoverykey"
@@ -41,27 +41,27 @@ dnf update --refresh
 dnf install kernel-devel kernel-headers gcc gcc-c++ make git
 
 # Clone modules source code
-pushd . > /dev/null
+pushd . >/dev/null
 cd /opt
 
-    if [[ -e vm-host-modules ]] ; then
-        echo "Modules source code folder detected at '/opt/vm-host-modules'. Continuing with installation will replace the folder, potentially causing data loss."
-        if yes-or-no "Continue? (replace folder)" ; then
-            rm -rf /opt/vm-host-modules
-        else
-            echo "Aborting..."
-            exit 2
-        fi
+if [[ -e vm-host-modules ]]; then
+    echo "Modules source code folder detected at '/opt/vm-host-modules'. Continuing with installation will replace the folder, potentially causing data loss."
+    if yes-or-no "Continue? (replace folder)"; then
+        rm -rf /opt/vm-host-modules
+    else
+        echo "Aborting..."
+        exit 2
     fi
+fi
 
-    git clone -b 17.6 https://github.com/bytium/vm-host-modules
-    cd vm-host-modules/
+git clone -b 17.6 https://github.com/bytium/vm-host-modules
+cd vm-host-modules/
 
-    make
-    make install
-    make clean
+make
+make install
+make clean
 
-popd > /dev/null
+popd >/dev/null
 
 # Install reinstallation script
 cp reinstall.sh /opt/vm-host-modules/
@@ -73,8 +73,8 @@ systemctl daemon-reload
 systemctl enable vm-host-modules-reinstall.service
 
 # Sign modules if needed
-if mokutil --sb-state | grep -q "SecureBoot enabled" ; then
-    if [[ -f "/etc/pki/akmods/private/private_key.priv" ]] ; then
+if mokutil --sb-state | grep -q "SecureBoot enabled"; then
+    if [[ -f "/etc/pki/akmods/private/private_key.priv" ]]; then
         echo "Signing modules for SecureBoot..."
         sudo "/usr/src/kernels/$(uname -r)/scripts/sign-file" sha256 /etc/pki/akmods/private/private_key.priv /etc/pki/akmods/certs/public_key.der $(modinfo -n vmmon)
         sudo "/usr/src/kernels/$(uname -r)/scripts/sign-file" sha256 /etc/pki/akmods/private/private_key.priv /etc/pki/akmods/certs/public_key.der $(modinfo -n vmnet)
