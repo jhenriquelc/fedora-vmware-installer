4f1b95f92d065591a46a2d671a98b59d0c8aa644
João H. L. Corrêa <jhenriquelc@gmail.com>
2025-01-30 15:07:43 -0300

    Format `reinstall.sh`

--- reinstall.sh
+++ reinstall.sh
@@ -9,19 +9,19 @@ if ! (modinfo vmmon vmnet); then # modules aren't installed
     pushd .
     cd /opt/vm-host-modules/ || exit 5
 
-        echo "Rebuilding and reinstalling modules..."
+    echo "Rebuilding and reinstalling modules..."
 
-        make clean
-        make && make install
-        make clean
+    make clean
+    make && make install
+    make clean
 
     popd || exit 6
 fi
 
-if ! (modprobe vmmon vmnet) ; then # modules aren't loaded
+if ! (modprobe vmmon vmnet); then # modules aren't loaded
     echo "Detected modules rejection, attempting fix for SecureBoot..."
-    if mokutil --sb-state | grep -q "SecureBoot enabled" ; then # modules aren't signed
-        if [[ ! -f "/etc/pki/akmods/private/private_key.priv" ]] ; then # can't sign modules
+    if mokutil --sb-state | grep -q "SecureBoot enabled"; then         # modules aren't signed
+        if [[ ! -f "/etc/pki/akmods/private/private_key.priv" ]]; then # can't sign modules
             echo "You're using secure boot but don't seem to have a self-signing key present. Please read the following guide to create and register a MOK:"
             echo "https://rpmfusion.org/Howto/Secure%20Boot?highlight=%28%5CbCategoryHowto%5Cb%29"
             echo "Note that installing a MOK will trip Bitlocker encryption if you have it enabled in a Windows dual-boot, please make sure you have access to your recovery keys: https://aka.ms/myrecoverykey"
